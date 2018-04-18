fed2c19b1d59208c89c852397e9cfc7fab5d9eee
OttoHein <33087686+OttoHein@users.noreply.github.com>
2018-04-18 16:01:30 +0200

    DB Logic
    Base DB Logic implemented

--- src/main/java/edu/ap/spring/jpa/Question.java
+++ src/main/java/edu/ap/spring/jpa/Question.java
@@ -1,7 +1,50 @@
 package edu.ap.spring.jpa;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Question {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String question;
+	private String answer;
+	
+	public Question(int id, String question, String answer) {
+		super();
+		this.id = id;
+		this.question = question;
+		this.answer = answer;
+	}
+
+	public Question() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
 }
--- src/main/java/edu/ap/spring/jpa/QuestionRepository.java
+++ src/main/java/edu/ap/spring/jpa/QuestionRepository.java
@@ -3,7 +3,51 @@ package edu.ap.spring.jpa;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import edu.ap.spring.jpa.Question;
+
 @Repository
 public interface QuestionRepository extends CrudRepository<Question, Long> {
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	class QuestionRowMapper implements RowMapper<Question>{
+		@Override
+		public Question mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Question question = new Question();
+			question.setId(rs.getInt("id"));
+			question.setQuestion(rs.getString("question"));
+			question.setAnswer(rs.getString("answer"));
+			return question;
+		}
+	
+		public List<Question> findAll() {
+			return jdbcTemplate.query("select * from question", new QuestionRowMapper());
+		}
+
+		public Question findById(int id) {
+			return jdbcTemplate.queryForObject("select * from question where id=?", new Object[] { id },
+					new BeanPropertyRowMapper<Question>(Question.class));
+		}
+
+		public int deleteById(int id) {
+			return jdbcTemplate.update("delete from question where id=?", new Object[] { id });
+		}
+
+		public int insert(Question question) {
+			return jdbcTemplate.update("insert into question (id, question, answer) " + "values(?,  ?, ?)",
+					new Object[] { question.getId(), question.getQuestion(), question.getAnswer() });
+		}	
+	}
 }
 
